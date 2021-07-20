a510d589e738a609aba39dafffce2a33a900aac0
unknown <rvchandrasekar5@gmail.com>
2021-07-20 17:11:34 +0530

    RelationalOperators

---
+++ RelationalOperators.java
@@ -0,0 +1,16 @@
+package com.pluralsight.organized;
+
+public class RelationalOperators{
+
+    public static void main(String[] args) {
+	  int a=10;
+	  int b=20;
+
+	  System.out.println(" a==b = " + (a==b) );
+	  System.out.println(" a!=b = " + (a!=b));
+	  System.out.println(" a>b = " + (a>b));
+	  System.out.println(" a<b = " + (a<b));
+	  System.out.println(" b>=a = " + (b>=a)  );
+	  System.out.println(" b<=a = " + (b<=a));
+    }
+}
